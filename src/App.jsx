bfedde548d7eb499274a1e56b1fa79adf784ba0c
aa1as <sm13929881@gmail.com>
2024-08-13 16:26:52 +0900

    [MainPage] 슬라이더, 카테고리 ui 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <>
       <main>
-        <div>Hello World</div>
+        <MainPage />
       </main>
     </>
   );
