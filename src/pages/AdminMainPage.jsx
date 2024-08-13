51de265317a0120b4c6a7a6b98a9588dc1a21b01
hbin3673 <hbin3673@gmail.com>
2024-08-13 12:40:20 +0900

    [AdminMainPage] 관리자메인 페이지에 공지사항과 예약 타이틀과 버튼 추가

--- src/pages/AdminMainPage.jsx
+++ src/pages/AdminMainPage.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/AdminMainPage.css";
 
 const AdminMainPage = () => {
+  const [focusedButton, setFocusedButton] = useState(null);
+
+  const handleFocus = (button) => {
+    setFocusedButton(button);
+  };
+
+  const handleBlur = () => {
+    setFocusedButton(null);
+  };
+
   return (
     <div>
       <div className="capmingTitle">캠핑지</div>
@@ -11,10 +21,48 @@ const AdminMainPage = () => {
       </div>
       <div className="staticsTitle">통계</div>
       <div className="staticsCategory">
-        <button className="staticsCategorySite">사이트</button>
-        <button className="staticsCategoryUser">회원 관리</button>
-        <button className="staticsCategoryReview">리뷰 관리</button>
-        <button className="staticsCategorySale">매출</button>
+        <button
+          className="staticsCategorySite"
+          onFocus={() => handleFocus("site")}
+          onBlur={handleBlur}
+        >
+          사이트
+        </button>
+        <button
+          className="staticsCategoryUser"
+          onFocus={() => handleFocus("user")}
+          onBlur={handleBlur}
+        >
+          회원 관리
+        </button>
+        <button
+          className="staticsCategoryReview"
+          onFocus={() => handleFocus("review")}
+          onBlur={handleBlur}
+        >
+          리뷰 관리
+        </button>
+        <button
+          className="staticsCategorySale"
+          onFocus={() => handleFocus("sale")}
+          onBlur={handleBlur}
+        >
+          매출
+        </button>
+      </div>
+      {focusedButton === "site" && (
+        <div className="imageContainer">
+          <img src="assets/Site.png" alt="Site image" />
+        </div>
+      )}
+      <div className="notiTitle">공지사항</div>
+      <div className="topBtn">
+        <button className="notiRegisterBtn">+ 등록</button>
+        <button className="notiCorrectBtn">수정</button>
+      </div>
+      <div className="reserveTitle">예약</div>
+      <div className="topBtn">
+        <button className="reserveSeeBtn">조회</button>
       </div>
     </div>
   );
