f9fb6b655232b59a1cbff75721fe57dafbd14d65
hbin3673 <hbin3673@gmail.com>
2024-08-13 11:24:00 +0900

    [App] 관리자메인페이지 import하고 테스트진행

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import AdminMainPage from "./pages/AdminMainPage";
 
 function App() {
   return (
     <>
       <main>
-        <div>Hello World</div>
+        <AdminMainPage />
       </main>
     </>
   );
