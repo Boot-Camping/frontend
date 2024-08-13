f022ed4b010cf2a8a47c6c355639c1f138c25a5c
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-13 12:54:21 +0900

    [App] 라우터 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import MyPage from "./pages/MyPage";
+import UserInfoPage from "./pages/UserInfoPage";
+import PaidPage from "./pages/PaidPage";
+import SavePage from "./pages/SavePage";
+import CashPage from "./pages/CashPage";
+import NoticePage from "./pages/NoticePage";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <main>
-        <div>Hello World</div>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/userinfo" element={<UserInfoPage />} />
+          <Route path="/paid" element={<PaidPage />} />
+          <Route path="/save" element={<SavePage />} />
+          <Route path="/cash" element={<CashPage />} />
+          <Route path="/notice" element={<NoticePage />} />
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
   );
 }
 
