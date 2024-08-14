19830a45e1795ff5d6f98505833e51e3e0ea3b1f
Sammyissewon <person1341@gmail.com>
2024-08-15 00:38:54 +0700

    [DetailPage] ImageSlider.jsx 코드 변경

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-
-import ImageSlider from "../components/ImageSlider";
 import DetailPageInfo from "./DetailPageInfo";
 import ReviewPage from "../components/ReviewPage";
 import BookButton from "../components/BookButton";
-import { Link } from "react-router-dom";
+import ImageSlider from "../components/imageSlider/ImageSlider";
+
 import "../css/DetailPage.css";
+
+const sliderData = [
+  "./assets/detailImg-1.png",
+  "./assets/detailImg-2.png",
+  "./assets/detailImg-3.png",
+  "./assets/detailImg-4.png",
+  "./assets/detailImg-5.png",
+  "./assets/detailImg-6.png",
+];
 
 const DetailPage = () => {
   return (
-    <>
-      <div className="detail-page">
-        <ImageSlider />
-        <DetailPageInfo />
-        <ReviewPage />
-        <Link to="/book">
-          <BookButton />
-        </Link>
-      </div>
-    </>
+    <div className="detail-page">
+      <ImageSlider sliderData={sliderData} />
+      <DetailPageInfo />
+      <ReviewPage />
+      <BookButton to="/book" />
+    </div>
   );
 };
 
