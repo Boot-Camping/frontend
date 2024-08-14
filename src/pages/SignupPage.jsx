4a75ee99ff117bfe370e65ea7155c702f41f7fc7
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-14 17:21:07 +0900

    [SignupPage.jsx] 에러 내용에 따라 모달에 다른 내용 띄우기

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -5,15 +5,21 @@ import SignupModal from "../components/sign-up/SignupModal";
 
 const SignupPage = () => {
   const [error, setError] = useState(false);
+  const [errorType, setErrorType] = useState("");
   const [isOpened, setIsOpened] = useState(false);
 
   return (
     <section className="signup-section">
       <div className="signup-title">회원가입</div>
-      <SignupForm setError={setError} setIsOpened={setIsOpened} />
+      <SignupForm
+        setError={setError}
+        setIsOpened={setIsOpened}
+        setErrorType={setErrorType}
+      />
 
       <SignupModal
         error={error}
+        errorType={errorType}
         isOpened={isOpened}
         setIsOpened={setIsOpened}
       />
