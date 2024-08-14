05476266b968d7c586d45ac6d411d86a7c014164
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-14 23:07:52 +0900

    [DaumPostCode.jsx] 다음우편번호 api 로직 유틸파일로 생성

--- src/components/sign-up/DaumPostCode.jsx
+++ src/components/sign-up/DaumPostCode.jsx
@@ -1,81 +1,23 @@
 import React, { useEffect, useState } from "react";
+import {
+  loadDaumPostCode,
+  openPostcodePopup,
+} from "../../utils/daumPostCodeUtils";
 
 const PostCodeApi = ({ postcode, setPostcode }) => {
   const [address, setAddress] = useState("");
   const [detailAddress, setDetailAddress] = useState("");
   const [extraAddress, setExtraAddress] = useState("");
-  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
 
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
-    script.async = true;
-
-    script.onload = () => {
-      setIsScriptLoaded(true);
-    };
-
-    document.body.appendChild(script);
-
+    const cleanup = loadDaumPostCode();
     return () => {
-      document.body.removeChild(script);
+      cleanup();
     };
   }, []);
 
-  const sample6_execDaumPostcode = (event) => {
-    event.preventDefault();
-
-    if (isScriptLoaded) {
-      new window.daum.Postcode({
-        oncomplete: function (data) {
-          // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
-
-          var addr = "";
-          var extraAddr = "";
-
-          if (data.userSelectedType === "R") {
-            addr = data.roadAddress;
-          } else {
-            addr = data.jibunAddress;
-          }
-
-          if (data.userSelectedType === "R") {
-            if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
-              extraAddr += data.bname;
-            }
-            if (data.buildingName !== "" && data.apartment === "Y") {
-              extraAddr +=
-                extraAddr !== "" ? ", " + data.buildingName : data.buildingName;
-            }
-            if (extraAddr !== "") {
-              extraAddr = " (" + extraAddr + ")";
-            }
-            setExtraAddress(extraAddr);
-          } else {
-            setExtraAddress("");
-          }
-
-          console.log("우편번호:", data.zonecode);
-          console.log("주소:", addr);
-
-          setPostcode(data.zonecode);
-          setAddress(addr);
-
-          document.getElementById("detail-address").focus();
-        },
-        theme: {
-          searchBgColor: "#038153",
-          queryTextColor: "#FFFFFF",
-          bgColor: "#EAEAEA",
-        },
-      }).open({
-        popupKey: "popup1",
-        popupTitle: "우편번호 찾기",
-      });
-    } else {
-      console.error("아직 스크립트가 로드되지 않음");
-    }
+  const postcodeSearchHandle = () => {
+    openPostcodePopup(setAddress, setPostcode, setExtraAddress);
   };
 
   return (
@@ -84,7 +26,7 @@ const PostCodeApi = ({ postcode, setPostcode }) => {
         <button
           id="postcode-btn"
           className="postcode-btn"
-          onClick={sample6_execDaumPostcode}
+          onClick={postcodeSearchHandle}
         >
           우편번호 찾기
         </button>
