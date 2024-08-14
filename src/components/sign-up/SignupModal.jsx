a50868dd15f8d2f5c51fac0172c35ab2984e013a
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-14 15:49:11 +0900

    [SignupModal.jsx] 회원가입 모달 생성 및 CSS 추가

--- src/components/sign-up/SignupModal.jsx
+++ src/components/sign-up/SignupModal.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { createPortal } from "react-dom";
 
-const SignupModal = ({ error, isOpened }) => {
+const SignupModal = ({ error, isOpened, setIsOpened }) => {
+  const closeHandle = () => {
+    setIsOpened(false);
+  };
+
   return (
-    <div>{error && <div>우편번호와 주소는 필수 입력 사항입니다.</div>}</div>
+    <>
+      {isOpened && (
+        <>
+          {createPortal(
+            <div className="overlay" onClick={closeHandle}></div>,
+            document.getElementById("overlay-root")
+          )}
+          {createPortal(
+            <div className="signup-modal signup-error">
+              <div className="signup-modal-content">
+                {error && (
+                  <ul>
+                    <li>우편번호와 주소는</li>
+                    <li>필수 입력 사항입니다.</li>
+                  </ul>
+                )}
+              </div>
+              <button
+                type="button"
+                className="signup-modal-btn"
+                onClick={closeHandle}
+              >
+                확인
+              </button>
+            </div>,
+            document.getElementById("modal-root")
+          )}
+        </>
+      )}
+    </>
   );
 };
 
