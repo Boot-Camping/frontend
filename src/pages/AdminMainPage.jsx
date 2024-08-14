96760410b0dcc39efb536046667d72a728ba509b
hbin3673 <hbin3673@gmail.com>
2024-08-14 11:33:50 +0900

    [AdminMainPage] 케밥케이스로 클래스 네임 변경

--- src/pages/AdminMainPage.jsx
+++ src/pages/AdminMainPage.jsx
@@ -14,36 +14,36 @@ const AdminMainPage = () => {
 
   return (
     <div>
-      <div className="capmingTitle">캠핑지</div>
-      <div className="topBtn">
-        <button className="campingRegisterBtn">+ 등록</button>
-        <button className="campingCorrectBtn">수정</button>
+      <div className="capming-title">캠핑지</div>
+      <div className="top-btn">
+        <button className="camping-register-btn">+ 등록</button>
+        <button className="camping-correct-btn">수정</button>
       </div>
-      <div className="staticsTitle">통계</div>
-      <div className="staticsCategory">
+      <div className="statics-title">통계</div>
+      <div className="statics-category">
         <button
-          className="staticsCategorySite"
+          className="statics-category-site"
           onFocus={() => handleFocus("site")}
           onBlur={handleBlur}
         >
           사이트
         </button>
         <button
-          className="staticsCategoryUser"
+          className="statics-category-user"
           onFocus={() => handleFocus("user")}
           onBlur={handleBlur}
         >
           회원 관리
         </button>
         <button
-          className="staticsCategoryReview"
+          className="statics-category-review"
           onFocus={() => handleFocus("review")}
           onBlur={handleBlur}
         >
           리뷰 관리
         </button>
         <button
-          className="staticsCategorySale"
+          className="statics-category-sale"
           onFocus={() => handleFocus("sale")}
           onBlur={handleBlur}
         >
@@ -51,18 +51,18 @@ const AdminMainPage = () => {
         </button>
       </div>
       {focusedButton === "site" && (
-        <div className="imageContainer">
+        <div className="image-container">
           <img src="assets/Site.png" alt="Site image" />
         </div>
       )}
-      <div className="notiTitle">공지사항</div>
-      <div className="topBtn">
-        <button className="notiRegisterBtn">+ 등록</button>
-        <button className="notiCorrectBtn">수정</button>
+      <div className="noti-title">공지사항</div>
+      <div className="top-btn">
+        <button className="noti-register-btn">+ 등록</button>
+        <button className="noti-correct-btn">수정</button>
       </div>
-      <div className="reserveTitle">예약</div>
-      <div className="topBtn">
-        <button className="reserveSeeBtn">조회</button>
+      <div className="reserve-title">예약</div>
+      <div className="top-btn">
+        <button className="reserve-see-btn">조회</button>
       </div>
     </div>
   );
