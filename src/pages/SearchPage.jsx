7d20ce1ef7f1f74f1054384d2f3303da23d1ccf6
aa1as <sm13929881@gmail.com>
2024-08-15 23:18:48 +0900

    [SearchPage.jsx] 지역선택 드롭박스, 검색 창, 버튼 구현

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import "../components/search-page/SearchPage.css";
 
 const SearchPage = () => {
-  return <div></div>;
+  // const searchSubmitHandle((e) => {
+
+  // })
+  return (
+    <>
+      <div className="search-title">검색</div>
+      <select>
+        <option value="all-area">전체 지역</option>
+        <option value="경기">경기</option>
+        <option value="인천">인천</option>
+        <option value="강원">강원</option>
+        <option value="대전">대전</option>
+        <option value="세종">세종</option>
+        <option value="충북">충북</option>
+        <option value="충남">충남</option>
+        <option value="대구">대구</option>
+        <option value="울산">울산</option>
+        <option value="경북">경북</option>
+        <option value="경남">경남</option>
+        <option value="전북">전북</option>
+        <option value="전남">전남</option>
+        <option value="제주">제주</option>
+      </select>
+      {/* <input
+        className="search-text"
+        type="text"
+        id="search"
+        placeholder="검색어 입력"
+        // onChange={searchSubmitHandle}
+      /> */}
+
+      <input
+        class="inputId"
+        type="text"
+        name="keyword"
+        placeholder="검색어 입력"
+      />
+
+      <input class="submitBtn" type="submit" value="검색하기" />
+    </>
+  );
 };
 
 export default SearchPage;
