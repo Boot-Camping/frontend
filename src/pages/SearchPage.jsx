9b916996769c5f7792767ce833d0f9c54334cca6
aa1as <sm13929881@gmail.com>
2024-08-16 14:23:34 +0900

    [SearchPage.jsx] input창 css 변경

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -6,33 +6,37 @@ const SearchPage = () => {
   return (
     <>
       <div className="search-title">검색</div>
-      <select className="search-area">
-        <option value="all-area">전체 지역</option>
-        <option value="경기">경기</option>
-        <option value="인천">인천</option>
-        <option value="강원">강원</option>
-        <option value="대전">대전</option>
-        <option value="세종">세종</option>
-        <option value="충북">충북</option>
-        <option value="충남">충남</option>
-        <option value="대구">대구</option>
-        <option value="울산">울산</option>
-        <option value="경북">경북</option>
-        <option value="경남">경남</option>
-        <option value="전북">전북</option>
-        <option value="전남">전남</option>
-        <option value="제주">제주</option>
-      </select>
-      <input
-        className="search-text"
-        type="text"
-        id="search"
-        placeholder="캠핑장 검색"
-        // value={searchText}
-        // onChange={searchSubmitHandle}
-      />
+      <div className="search">
+        <select className="search-area">
+          <option value="all-area">전체 지역</option>
+          <option value="경기">경기</option>
+          <option value="인천">인천</option>
+          <option value="강원">강원</option>
+          <option value="대전">대전</option>
+          <option value="세종">세종</option>
+          <option value="충북">충북</option>
+          <option value="충남">충남</option>
+          <option value="대구">대구</option>
+          <option value="울산">울산</option>
+          <option value="경북">경북</option>
+          <option value="경남">경남</option>
+          <option value="전북">전북</option>
+          <option value="전남">전남</option>
+          <option value="제주">제주</option>
+        </select>
+        <input
+          className="search-text"
+          type="text"
+          id="search"
+          placeholder="캠핑장 검색"
+          // value={searchText}
+          // onChange={searchSubmitHandle}
+        />
 
-      <input className="search-submit-Btn" type="submit" value="검색" />
+        <input className="search-submit-Btn" type="submit" value="검색" />
+      </div>
+
+      <div className="search-block"></div>
     </>
   );
 };
