99a76ee482bec4248ba9bf4aea43e1d9c5edd7cc
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-17 17:47:47 +0900

    [UserAccount.jsx] 모달 open 및 타입 상태관리 로직 추가

--- src/components/user-info-page/UserAccount.jsx
+++ src/components/user-info-page/UserAccount.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { userInfoIcon, userProfile } from "../../constants/userInfo";
 import { ReactSVG } from "react-svg";
 
-const UserAccount = () => {
+const UserAccount = ({ setIsOpened, setModalType }) => {
+  const pwChangeHandle = () => {
+    setIsOpened(true);
+    setModalType("password");
+    console.log("비밀번호 변경");
+  };
+
   return (
     <div className="user-account-wrap profile-txt-wrap underline">
       <div className="user-account profile-txt">
         <div>아이디</div>
         <div>{userProfile.loginId}</div>
       </div>
-      <div className="user-account profile-txt">
+      <div className="user-account profile-txt" onClick={pwChangeHandle}>
         <div>비밀번호</div>
         <div>
           <div>*******</div>
