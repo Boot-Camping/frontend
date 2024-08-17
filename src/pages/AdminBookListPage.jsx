7ca0dfdc85d3a35d869dc51e5a7a3de490a9290d
hbin3673 <hbin3673@gmail.com>
2024-08-17 17:04:06 +0900

    [AdminBookListPage] 예약 조회 리스트 페이지 구성(보이는 부분만)

---
+++ src/pages/AdminBookListPage.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "../components/admin-book-page/AdminBookListPage.css";
+
+const AdminBookListPage = () => {
+  return (
+    <div>
+      <div className="admin-book-title">예약 조회</div>
+      <div className="book-list-ex">
+        <div>[캠핑지 이름] 8/12~8/16(금) 닉네임 n명 </div>
+        <span className="book-list-chevron">
+          <img src="../../src/assets/svg/chevron-right.svg" alt="" />
+        </span>
+      </div>
+      <div className="book-list-ex">
+        <div>[캠핑지 이름] 8/12~8/16(금) 닉네임 n명 </div>
+        <span className="book-list-chevron">
+          <img src="../../src/assets/svg/chevron-right.svg" alt="" />
+        </span>
+      </div>
+      <div className="book-list-ex">
+        <div>[캠핑지 이름] 8/12~8/16(금) 닉네임 n명 </div>
+        <span className="book-list-chevron">
+          <img src="../../src/assets/svg/chevron-right.svg" alt="" />
+        </span>
+      </div>
+      <div className="book-list-ex">
+        <div>[캠핑지 이름] 8/12~8/16(금) 닉네임 n명 </div>
+        <span className="book-list-chevron">
+          <img src="../../src/assets/svg/chevron-right.svg" alt="" />
+        </span>
+      </div>
+      <div className="book-list-ex">
+        <div>[캠핑지 이름] 8/12~8/16(금) 닉네임 n명 </div>
+        <span className="book-list-chevron">
+          <img src="../../src/assets/svg/chevron-right.svg" alt="" />
+        </span>
+      </div>
+      <div className="book-list-ex">
+        <div>[캠핑지 이름] 8/12~8/16(금) 닉네임 n명 </div>
+        <span className="book-list-chevron">
+          <img src="../../src/assets/svg/chevron-right.svg" alt="" />
+        </span>
+      </div>
+      <div className="book-list-ex">
+        <div>[캠핑지 이름] 8/12~8/16(금) 닉네임 n명 </div>
+        <span className="book-list-chevron">
+          <img src="../../src/assets/svg/chevron-right.svg" alt="" />
+        </span>
+      </div>
+      <div className="book-list-ex">
+        <div>[캠핑지 이름] 8/12~8/16(금) 닉네임 n명 </div>
+        <span className="book-list-chevron">
+          <img src="../../src/assets/svg/chevron-right.svg" alt="" />
+        </span>
+      </div>
+
+      <div className="book-list-container">
+        <button className="book-list-plus">더보기</button>
+      </div>
+    </div>
+  );
+};
+
+export default AdminBookListPage;
