410249ebee8c1e5da6d3c156848672b5cc60cf94
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-18 23:03:46 +0900

    [save.jsx] 찜목록 상수데이터 분리 및 이미지 import

---
+++ src/constants/save.jsx
@@ -0,0 +1,56 @@
+/*****************************
+ * 	이미지 불러오기
+ */
+import prev from "/src/assets/svg/arrowNext.svg";
+import heart from "/src/assets/svg/heart.svg";
+
+export const saveIcon = {
+  prev,
+  heart,
+};
+
+/*****************************
+ * 	찜목록 데이터
+ */
+export const saveData = [
+  {
+		wish: true,
+    campImage: "/src/assets/image/detailImg-1.png",
+    campName: "캠프그라운드 화온",
+    price: 40000,
+    addr: "충남 아산시 영인면 고룡산로 512-37",
+    standardNum: 4,
+    maxNum: 6,
+    overCharge: 10000,
+  },
+  {
+		wish: true,
+    campImage: "/src/assets/image/detailImg-2.png",
+    campName: "파이브이모션",
+    price: 50000,
+    addr: "경기 용인시 처인구 백암면 고안리 51-5",
+    standardNum: 3,
+    maxNum: 5,
+    overCharge: 20000,
+  },
+  {
+		wish: true,
+    campImage: "/src/assets/image/detailImg-3.png",
+    campName: "캠프그라운드 화온",
+    price: 40000,
+    addr: "충남 아산시 영인면 고룡산로 512-37",
+    standardNum: 4,
+    maxNum: 6,
+    overCharge: 10000,
+  },
+  {
+		wish: true,
+    campImage: "/src/assets/image/detailImg-4.png",
+    campName: "파이브이모션",
+    price: 50000,
+    addr: "경기 용인시 처인구 백암면 고안리 51-5",
+    standardNum: 3,
+    maxNum: 5,
+    overCharge: 20000,
+  },
+];
