ed197b18ce843a3a8f2249dbeaaaf666f0d3cc6e
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-18 17:10:03 +0900

    [DeleteConfirm.jsx] 비밀번호 입력창, 체크박스 UI 구현 및 CSS 적용

--- src/components/delete-account-page/DeleteConfirm.jsx
+++ src/components/delete-account-page/DeleteConfirm.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import "./DeleteConfirm.css";
+import { ReactSVG } from "react-svg";
+import check from "/src/assets/svg/check.svg";
 
-const DeleteConfirm = () => {
+const DeleteConfirm = ({ passwordRef, checkboxRef }) => {
   return (
     <>
       <div className="delete-confirm">
         <label>비밀번호 입력</label>
-        <input type="password" placeholder="현재 비밀번호를 입력해주세요" />
+        <input
+          type="password"
+          name="password"
+          placeholder="현재 비밀번호를 입력해주세요"
+          required
+          ref={passwordRef}
+        />
       </div>
 
-      <div>
-        <label>위 내용을 모두 확인하였으며, 이에 동의합니다</label>
-        <input type="checkbox" />
+      <div className="delete-confirm-chk">
+        <input type="checkbox" id="delete-chk" ref={checkboxRef} />
+        <label htmlFor="delete-chk">
+          <ReactSVG src={check} className="delete-chk-img" />
+          <div>위 내용을 모두 확인하였으며, 이에 동의합니다</div>
+        </label>
       </div>
     </>
   );
