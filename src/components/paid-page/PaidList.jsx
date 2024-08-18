c57104538cb2fd8df8f16bbd87b65f3f27d5132c
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-18 21:37:10 +0900

    [PaidList.jsx] 예약/이용 완료에 따라 다른내용 띄우기 및 CSS 적용

---
+++ src/components/paid-page/PaidList.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./PaidList.css";
+import { paidData, paidIcon } from "../../constants/paid";
+import { ReactSVG } from "react-svg";
+
+const PaidList = () => {
+  const renderButton = (data, index) => {
+    if (data.bookStatus === "예약 완료") {
+      return (
+        <button>
+          <ReactSVG src={paidIcon.cancel} className="paid-list-img" />
+          예약 취소
+        </button>
+      );
+    } else {
+      return (
+        <button>
+          <ReactSVG src={paidIcon.write} className="paid-list-img" />
+          리뷰 작성
+        </button>
+      );
+    }
+  };
+
+  return (
+    <div className="paid-list-wrap">
+      {paidData.map((data, index) => (
+        <div
+          className={`paid-list ${
+            data.bookStatus === "예약 완료" ? "book-list" : "usage-list"
+          } `}
+          key={`paid-list-${index}`}
+        >
+          <div className="paid-list-status">
+            <div className="paid-status">{data.bookStatus}</div>
+            <div className="paid-period">
+              <span>{data.startDate}</span> ~ <span>{data.endDate}</span>
+            </div>
+          </div>
+          <div className="paid-list-price">
+            <div>{data.campName}</div>
+            <div>{data.totalPrice}원</div>
+          </div>
+          <div className="paid-list-personnel">총 {data.bookNum}명</div>
+          <div className="paid-list-request">
+            <div>요청 사항</div>
+            <div>{data.bookRequest}</div>
+          </div>
+          <div className="paid-list-btn">{renderButton(data, index)}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PaidList;
