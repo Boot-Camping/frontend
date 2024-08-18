936d941c1fde70fbfa48392792565eac52640398
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-18 21:40:17 +0900

    [PaidPage.jsx] 결제내역 UI 구현 및 CSS 적용

--- src/pages/PaidPage.jsx
+++ src/pages/PaidPage.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import "../components/paid-page/PaidPage.css";
+import { ReactSVG } from "react-svg";
+import { paidIcon } from "../constants/paid";
+import PaidFilter from "../components/paid-page/PaidFilter";
+import PaidList from "../components/paid-page/PaidList";
+import { Link } from "react-router-dom";
 
 const PaidPage = () => {
-	return (
-		<div>
-			구매내역
-		</div>
-	);
+  return (
+    <section className="paid-wrap">
+      <div className="paid-title-wrap">
+        <Link to={"/mypage"}>
+          <ReactSVG src={paidIcon.prev} className="paid-move-prev" />
+        </Link>
+        <div>결제 내역</div>
+      </div>
+
+      <PaidFilter />
+
+      <PaidList />
+    </section>
+  );
 };
 
 export default PaidPage;
