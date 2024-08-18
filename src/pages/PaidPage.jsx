545ed184977272bcaead8113f75bd0e4f5e9bb5d
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-18 21:49:15 +0900

    [PaidPage.jsx] 필터링 기능 추가

--- src/pages/PaidPage.jsx
+++ src/pages/PaidPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../components/paid-page/PaidPage.css";
 import { ReactSVG } from "react-svg";
 import { paidIcon } from "../constants/paid";
@@ -7,6 +7,12 @@ import PaidList from "../components/paid-page/PaidList";
 import { Link } from "react-router-dom";
 
 const PaidPage = () => {
+  const [filter, setFilter] = useState("all");
+
+  const filterChangeHandle = (status) => {
+    setFilter(status);
+  };
+
   return (
     <section className="paid-wrap">
       <div className="paid-title-wrap">
@@ -16,9 +22,9 @@ const PaidPage = () => {
         <div>결제 내역</div>
       </div>
 
-      <PaidFilter />
+      <PaidFilter filterChangeHandle={filterChangeHandle} />
 
-      <PaidList />
+      <PaidList filter={filter} />
     </section>
   );
 };
