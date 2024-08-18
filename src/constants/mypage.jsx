0bbb20a13bc16c79c0fafcbe136db321082511c5
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-18 21:53:31 +0900

    [MyPage.jsx] Link 경로 상수데이터 분리

--- src/constants/mypage.jsx
+++ src/constants/mypage.jsx
@@ -36,21 +36,25 @@ export const mypageImgs = {
 export const mypageBtns = [
   {
     key: "mypage-btn-paid",
+    link: "/paid",
     src: "calendar",
     txt: "결제 내역",
   },
   {
     key: "mypage-btn-review",
+    link: "/",
     src: "pencilSquare",
     txt: "나의 리뷰",
   },
   {
     key: "mypage-btn-save",
+    link: "/save",
     src: "heart",
     txt: "찜 목록",
   },
   {
     key: "mypage-btn-cash",
+    link: "/cash",
     src: "cash",
     txt: "캐시 충전/사용",
   },
