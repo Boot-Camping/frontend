9b413e6bef8409e3b7f36904b3feb0351dcee969
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-18 21:49:37 +0900

    [PaidFilter.jsx] 필터링 기능 추가 및 CSS 수정

--- src/components/paid-page/PaidFilter.jsx
+++ src/components/paid-page/PaidFilter.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "./PaidFilter.css";
 
-const PaidFilter = () => {
+const PaidFilter = ({ filterChangeHandle }) => {
   return (
     <div className="paid-filter">
-      <button className="book-filter">예약 완료</button>
-      <button className="usage-filter">이용 완료</button>
+      <button
+        className="usage-filter"
+        onClick={() => filterChangeHandle("all")}
+      >
+        전체 보기
+      </button>
+      <button
+        className="book-filter"
+        onClick={() => filterChangeHandle("예약 완료")}
+      >
+        예약 완료
+      </button>
+      <button
+        className="usage-filter"
+        onClick={() => filterChangeHandle("이용 완료")}
+      >
+        이용 완료
+      </button>
     </div>
   );
 };
