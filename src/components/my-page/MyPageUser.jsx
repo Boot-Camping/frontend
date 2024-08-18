3ac4c9826e6b9cef03a91a653f41c4e6f7feb22b
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-18 14:36:02 +0900

    [MyPageUser.jsx] CSS 파일 분리 및 import

--- src/components/my-page/MyPageUser.jsx
+++ src/components/my-page/MyPageUser.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./MyPageUser.css";
 import { mypageImgs, myPageData } from "../../constants/mypage";
 import { ReactSVG } from "react-svg";
 import { Link } from "react-router-dom";
