786494d29f602b7422bb7c78046aa51b7f580af1
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-18 23:09:12 +0900

    [SavePage.css] 찜목록 UI 구현 및 CSS 적용

--- src/pages/SavePage.jsx
+++ src/pages/SavePage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "../components/save-page/SavePage.css";
+import { saveIcon } from "../constants/save";
+import { Link } from "react-router-dom";
+import { ReactSVG } from "react-svg";
+import SaveList from "../components/save-page/SaveList";
+import SaveMoreBtn from "../components/save-page/SaveMoreBtn";
 
 const SavePage = () => {
-  return <div>찜목록</div>;
+  return (
+    <section className="save-wrap">
+      <div className="save-title-wrap">
+        <Link to={"/mypage"}>
+          <ReactSVG src={saveIcon.prev} className="save-move-prev" />
+        </Link>
+        <div>찜 목록</div>
+      </div>
+
+      <SaveList />
+
+      <SaveMoreBtn />
+    </section>
+  );
 };
 
 export default SavePage;
