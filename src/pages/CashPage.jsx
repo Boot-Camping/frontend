f3186bb289057ba025fb2fed7280d941a10c0d3e
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-19 16:02:29 +0900

    [CashPage.jsx] 충전/사용 페이지 UI 구현 및 CSS 적용

--- src/pages/CashPage.jsx
+++ src/pages/CashPage.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import "../components/cash-page/CashPage.css";
+import { Link } from "react-router-dom";
+import { ReactSVG } from "react-svg";
+import { cashIcon } from "../constants/cash";
+import CashFilter from "../components/cash-page/CashFilter";
+import CashList from "../components/cash-page/CashList";
+import CashChargeBtn from "../components/cash-page/CashChargeBtn";
 
 const CashPage = () => {
-	return (
-		<div>
-			캐시 충전/사용 내역 & 충전
-		</div>
-	);
+  return (
+    <section className="cash-page-wrap">
+      <div className="cash-title-wrap">
+        <Link to={"/mypage"}>
+          <ReactSVG src={cashIcon.prev} className="cash-move-prev" />
+        </Link>
+        <div>캐시 충전/사용 내역</div>
+      </div>
+
+      <CashFilter />
+
+      <CashList />
+
+      <CashChargeBtn />
+    </section>
+  );
 };
 
 export default CashPage;
