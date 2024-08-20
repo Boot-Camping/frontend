80f0d08ed557492bb620dfb3c28f9827b21e59c4
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-20 22:56:57 +0900

    [NoticePage.jsx] 필터 데이터 재사용 컴포넌트에 넘겨주기 및 CSS 파일 import

--- src/pages/NoticePage.jsx
+++ src/pages/NoticePage.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "../components/notice-page/NoticePage.css";
+import "../components/notice-page/NoticeFilter.css";
 import { Link } from "react-router-dom";
 import { ReactSVG } from "react-svg";
 import NoticeList from "../components/notice-page/NoticeList";
@@ -7,9 +8,16 @@ import { saveIcon } from "../constants/save";
 import SaveMoreBtn from "../components/save-page/SaveMoreBtn";
 import { useLoadMore } from "../hooks/useLoadMore";
 import { noticeData } from "../constants/notice";
+import PaidFilter from "../components/paid-page/PaidFilter";
+import { filterType } from "../constants/filterType";
 
 const NoticePage = () => {
   const { visibleItems, loadMore, hasMoreItems } = useLoadMore(6, noticeData);
+  const [filter, setFilter] = useState("all");
+
+  const filterChangeHandle = (status) => {
+    setFilter(status);
+  };
 
   return (
     <section className="notice-page-wrap">
@@ -17,10 +25,21 @@ const NoticePage = () => {
         <Link to={"/mypage"}>
           <ReactSVG src={saveIcon.prev} className="notice-move-prev" />
         </Link>
-        <div>공지사항</div>
+        <div>공지사항 및 이벤트</div>
       </div>
 
-      <NoticeList visibleItems={visibleItems} noticeData={noticeData} />
+      <PaidFilter
+        filterChangeHandle={filterChangeHandle}
+        filterType={filterType.notice}
+        wrapClassName="notice-page-filter"
+        allClassName="event-filter"
+      />
+
+      <NoticeList
+        visibleItems={visibleItems}
+        noticeData={noticeData}
+        filter={filter}
+      />
 
       <SaveMoreBtn onClick={loadMore} hasMoreItems={hasMoreItems} />
     </section>
