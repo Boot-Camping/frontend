75174cace0dfe13d0400661cc89e300c0d985421
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-20 20:34:38 +0900

    [App.jsx] 공지사항 상세페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import PaidPage from "./pages/PaidPage";
 import SavePage from "./pages/SavePage";
 import CashPage from "./pages/CashPage";
 import NoticePage from "./pages/NoticePage";
+import NoticeDetailPage from "./pages/NoticeDetailPage";
 import AdminMainPage from "./pages/AdminMainPage";
 import AdminCampingRegisterPage from "./pages/AdminCampingRegisterPage";
 import AdminCampFixPage from "./pages/AdminCampFixPage";
@@ -43,6 +44,7 @@ function App() {
           <Route path="/save" element={<SavePage />} />
           <Route path="/cash" element={<CashPage />} />
           <Route path="/notice" element={<NoticePage />} />
+          <Route path="/notice/:id" element={<NoticeDetailPage />} />
           <Route path="/detail" element={<DetailPage />} />
           <Route path="/book" element={<BookPage />} />
           <Route path="/book" element={<BookingPaymentPage />} />
