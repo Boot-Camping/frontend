c294d388c7e6d6648180515d38f816eca27a80f4
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-20 17:42:42 +0900

    [NoticePage.jsx] 공지사항 목록 UI 구현 및 CSS 적용

--- src/pages/NoticePage.jsx
+++ src/pages/NoticePage.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import "../components/notice-page/NoticePage.css";
+import { Link } from "react-router-dom";
+import { ReactSVG } from "react-svg";
+import NoticeList from "../components/notice-page/NoticeList";
+import { saveIcon } from "../constants/save";
 
 const NoticePage = () => {
-	return (
-		<div>
-			공지사항
-		</div>
-	);
+  return (
+    <section className="notice-page-wrap">
+      <div className="notice-title-wrap">
+        <Link to={"/mypage"}>
+          <ReactSVG src={saveIcon.prev} className="notice-move-prev" />
+        </Link>
+        <div>공지사항</div>
+      </div>
+
+      <NoticeList />
+    </section>
+  );
 };
 
 export default NoticePage;
