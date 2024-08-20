ab3b2e082644827baca06879eec171f6a1b03151
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-20 12:58:08 +0900

    [cash.jsx] 캐시 충전 모달에 상수데이터 추가

--- src/constants/cash.jsx
+++ src/constants/cash.jsx
@@ -28,3 +28,29 @@ export const cashData = [
     totalCash: 20000,
   },
 ];
+
+/*****************************
+ * 	캐시 충전 모달
+ */
+export const chargePrice = [
+  {
+    priceBtn: "+ 3만원",
+    price: 30000,
+  },
+  {
+    priceBtn: "+ 5만원",
+    price: 50000,
+  },
+  {
+    priceBtn: "+ 10만원",
+    price: 100000,
+  },
+  {
+    priceBtn: "+ 30만원",
+    price: 300000,
+  },
+  {
+    priceBtn: "+ 50만원",
+    price: 500000,
+  },
+];
