552c31cb0ad1d9596b32b9c8a91f8904679dcb4b
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-20 20:36:26 +0900

    [NoticeList.jsx] 공지 상세페이지 연결 및 CSS 적용
    - 날짜 형식 유틸함수로 분리

--- src/components/notice-page/NoticeList.jsx
+++ src/components/notice-page/NoticeList.jsx
@@ -1,34 +1,30 @@
 import React from "react";
 import "./NoticeList.css";
 import { noticeData } from "../../constants/notice";
+import { Link } from "react-router-dom";
+import { shortDateDot } from "../../utils/shortDateDot";
 
 const NoticeList = () => {
-  const shortDate = (data) => {
-    const date = data.createdAt;
-    const year = date.slice(0, 4);
-    const month = date.slice(5, 7);
-    const day = date.slice(8, 10);
-    return `${year}.${month}.${day}`;
-  };
-
   return (
     <div className="notice-list-wrap">
       {noticeData.map((data, index) => (
-        <>
-          <div key={`notice-list-${index + 1}`} className="notice-list">
-            <div className="notice-info">
-              <div
-                className={`notice-status ${
-                  data.noticeStatus === "이벤트" ? "notice-event" : ""
-                }`}
-              >
-                {data.noticeStatus}
-              </div>
-              <div>{data.title}</div>
+        <Link
+          to={`/notice/${data.id}`}
+          key={`notice-list-${index + 1}`}
+          className="notice-list"
+        >
+          <div className="notice-info">
+            <div
+              className={`notice-status ${
+                data.noticeStatus === "이벤트" ? "notice-event" : ""
+              }`}
+            >
+              {data.noticeStatus}
             </div>
-            <div className="notice-date">{shortDate(data)}</div>
+            <div>{data.title}</div>
           </div>
-        </>
+          <div className="notice-date">{shortDateDot(data)}</div>
+        </Link>
       ))}
     </div>
   );
