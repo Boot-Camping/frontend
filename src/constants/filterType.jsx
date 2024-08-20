29001aeb50e1525c48c8be4a6256937e1f181ad6
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-20 22:27:25 +0900

    [filterType.jsx] 결제내역 필터 데이터 상수데이터로 분리

---
+++ src/constants/filterType.jsx
@@ -0,0 +1,12 @@
+export const filterType = {
+  paid: [
+    {
+      type: "예약 완료",
+      class: "book-filter",
+    },
+    {
+      type: "이용 완료",
+      class: "usage-filter",
+    },
+  ],
+};
