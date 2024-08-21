fa19acb40dc1398f480714b0ea8ae522935ce07d
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-21 21:43:04 +0900

    [SignupForm.jsx] 유효성검사 및 전화번호 형식 유틸함수 추가

--- src/components/sign-up-page/SignupForm.jsx
+++ src/components/sign-up-page/SignupForm.jsx
@@ -4,6 +4,9 @@ import { signUp, signUpTerms } from "../../constants/signUp";
 import DaumPostCode from "./DaumPostCode";
 import SignupTerms from "../../components/sign-up-page/SignupTerms";
 import useAddress from "../../hooks/useAddress";
+import { phoneNumber } from "../../utils/phoneNumber";
+import { validation } from "../../utils/validation";
+import { hashPassword } from "../../utils/hashPassword";
 
 const SignupForm = ({ setError, setErrorType, setIsOpened, handleSubmit }) => {
   const { postcode, setPostcode } = useAddress();
@@ -14,31 +17,8 @@ const SignupForm = ({ setError, setErrorType, setIsOpened, handleSubmit }) => {
   const addressRef = useRef(null);
   const detailAddressRef = useRef(null);
 
-  const submitHandle = (event) => {
+  const submitHandle = async (event) => {
     event.preventDefault();
-
-    if (postcode === "") {
-      setError(true);
-      setErrorType("post");
-      setIsOpened(true);
-      return;
-    }
-
-    if (!checkedTerms.every(Boolean)) {
-      setError(true);
-      setErrorType("terms");
-      setIsOpened(true);
-      checkboxRefs.current.forEach((ref, index) => {
-        if (ref && !checkedTerms[index]) {
-          ref.focus();
-          return;
-        }
-      });
-      return;
-    }
-
-    setError(false); // 에러가 없으면 메시지 초기화
-    setIsOpened(false);
 
     // 입력된 데이터를 formData에 저장
     const formData = {};
@@ -52,8 +32,31 @@ const SignupForm = ({ setError, setErrorType, setIsOpened, handleSubmit }) => {
     });
     formData.addr = `(${postcode}) ${addressRef.current.value} ${detailAddressRef.current.value}`; // 주소는 별도로 처리
 
+    if (
+      !validation({
+        password: formData.password,
+        passwordChk: formData.passwordChk,
+        email: formData.email,
+        postcode,
+        checkedTerms,
+        checkboxRefs,
+        setError,
+        setErrorType,
+        setIsOpened,
+      })
+    ) {
+      return;
+    }
+
+    setError(false); // 에러가 없으면 메시지 초기화
+    setIsOpened(false);
+
+    formData.password = hashPassword(formData.password);
+    console.log(hashPassword(formData.password));
+    delete formData.passwordChk;
+
     // handleSubmit 호출
-    handleSubmit(formData);
+    // handleSubmit(formData);
 
     console.log("제출 완료", formData);
   };
@@ -78,6 +81,9 @@ const SignupForm = ({ setError, setErrorType, setIsOpened, handleSubmit }) => {
                 placeholder={signup.placeholder}
                 name={signup.key}
                 required
+                onInput={
+                  signup.label === "전화번호" ? (e) => phoneNumber(e) : null
+                }
               />
             )}
           </div>
