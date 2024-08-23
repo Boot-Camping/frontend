1dbab9b6796e54d871c6955b1056707f5ffb81dc
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-23 22:53:38 +0900

    [SaveList.jsx] 찜목록 데이터 띄우기

--- src/components/save-page/SaveList.jsx
+++ src/components/save-page/SaveList.jsx
@@ -3,7 +3,7 @@ import "./SaveList.css";
 import { saveIcon } from "../../constants/save";
 import { ReactSVG } from "react-svg";
 
-const SaveList = ({ visibleItems, saveData }) => {
+const SaveList = ({ saveData }) => {
   const [savedItems, setSavedItems] = useState(
     Array(saveData.length).fill(true)
   );
@@ -20,15 +20,15 @@ const SaveList = ({ visibleItems, saveData }) => {
 
   return (
     <div className="save-list-wrap">
-      {saveData.slice(0, visibleItems).map((data, index) => (
+      {saveData.map((data, index) => (
         <div className="save-list" key={`save-list${index + 1}`}>
           <div className="save-img">
-            <img src={data.campImage} />
+            <img src={data.campImages[0]} />
             <div className="save-addr">{shortAddr(data.addr)}</div>
           </div>
           <div className="save-txt-wrap">
             <div className="save-name-wrap">
-              <div className="save-name">{data.campName}</div>
+              <div className="save-name">{data.name}</div>
               <ReactSVG
                 src={saveIcon.heart}
                 className={`save-heart-img ${
@@ -37,10 +37,10 @@ const SaveList = ({ visibleItems, saveData }) => {
                 onClick={() => saveHandle(index)}
               />
             </div>
-            <div>1박 : {data.price.toLocaleString()}원</div>
-            <div>기준인원 : {data.standardNum}명</div>
-            <div>최대인원 : {data.maxNum}명</div>
-            <div>인당 추가요금 : {data.overCharge.toLocaleString()}원</div>
+            <div>1박 : {Number(data.price).toLocaleString()}원</div>
+            {/* <div>기준인원 : {data.standardNum}명</div> */}
+            {/* <div>최대인원 : {data.maxNum}명</div> */}
+            {/* <div>인당 추가요금 : {data.overCharge.toLocaleString()}원</div> */}
           </div>
         </div>
       ))}
