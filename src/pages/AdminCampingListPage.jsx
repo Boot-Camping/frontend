a7a083e030dfd6268360f02301514e52475f88d0
hbin3673 <hbin3673@gmail.com>
2024-08-25 12:11:05 +0900

    [AdminCampingListPage.jsx] 모든 캠핑지의 리스트를 백엔드에서 get 해오려고 했는데 서버 문제로 실패하여 원래 코드에 더보기 버튼만 추가

--- src/pages/AdminCampingListPage.jsx
+++ src/pages/AdminCampingListPage.jsx
@@ -1,3 +1,70 @@
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { ReactSVG } from "react-svg";
+// import { filterData } from "../utils/filterData";
+// import { useLoadMore } from "../hooks/useLoadMore";
+// import { noticeData } from "../constants/notice";
+// import useAllCampInfo from "../hooks/useAllCampInfo";
+// import useCampInfo from "../hooks/useCampInfo";
+// import SaveMoreBtn from "../components/save-page/SaveMoreBtn";
+
+// const AdminCampingListPage = () => {
+//   const [filter, setFilter] = useState("all");
+//   const filteredItems = filterData(noticeData, filter, "noticeStatus");
+//   const { visibleItems, loadMore, hasMoreItems } = useLoadMore(
+//     3,
+//     filteredItems
+//   );
+//   const { campInfos, loading, error } = useAllCampInfo(); // campId 제거
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>캠핑장 정보 가져오기 실패: {error.message}</div>;
+//   }
+
+//   return (
+//     <div>
+//       <Link to={"/admin"}>
+//         <ReactSVG
+//           className="admin-home-icon"
+//           src="../../src/assets/svg/home.svg"
+//           alt=""
+//         />
+//       </Link>
+//       <div className="admin-camping-list-title">캠핑장 리스트</div>
+//       <div className="admin-camping-list-wrapper">
+//         {campInfos.map((campInfo) => (
+//           <div key={campInfo.id} className="camping-list">
+//             <Link to={`/admin/camp-fix/${campInfo.id}`}>
+//               <div className="admin-camping-name-list">
+//                 <div>
+//                   {campInfo.name}{" "}
+//                   <div className="admin-camping-date-list">
+//                     {campInfo.createdAt}
+//                   </div>
+//                 </div>
+//                 <ReactSVG
+//                   className="list-pencil"
+//                   src="../../src/assets/svg/pencil.svg"
+//                   alt=""
+//                 />
+//               </div>
+//             </Link>
+//           </div>
+//         ))}
+//       </div>
+//       {hasMoreItems && (
+//         <SaveMoreBtn onClick={loadMore} hasMoreItems={hasMoreItems} />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AdminCampingListPage;
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ReactSVG } from "react-svg";
@@ -9,14 +76,14 @@ import SaveMoreBtn from "../components/save-page/SaveMoreBtn";
 import useCampingPlaceFilter from "../hooks/useCampingPlaceFilter";
 
 const AdminCampingListPage = () => {
+  const { selectedFilter, setSelectedFilter, campingPlaceFiltered } =
+    useCampingPlaceFilter(saveData);
   const [filter, setFilter] = useState("all");
   const filteredItems = filterData(noticeData, filter, "noticeStatus");
   const { visibleItems, loadMore, hasMoreItems } = useLoadMore(
     3,
     filteredItems
   );
-  const { selectedFilter, setSelectedFilter, campingPlaceFiltered } =
-    useCampingPlaceFilter(saveData);
   return (
     <div>
       <Link to={"/admin"}>
@@ -56,65 +123,3 @@ const AdminCampingListPage = () => {
 };
 
 export default AdminCampingListPage;
-
-// import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import { get } from "../utils/Api"; // API 호출 함수
-// import { ReactSVG } from "react-svg";
-// import useCampingPlaceFilter from "../hooks/useCampingPlaceFilter";
-// import SaveMoreBtn from "../components/save-page/SaveMoreBtn"; // 필요에 따라 컴포넌트 수정
-
-// const AdminCampingListPage = () => {
-//   const [campingPlaces, setCampingPlaces] = useState([]);
-
-//   const fetchCampingPlaces = async () => {
-//     try {
-//       const data = await get("camp"); // 엔드포인트 /api/camp 호출
-//       setCampingPlaces(
-//         data.map(({ name, createdAt }) => ({ name, createdAt }))
-//       );
-//     } catch (error) {
-//       console.error("Error fetching camping places: ", error.message);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchCampingPlaces();
-//   }, []);
-
-//   return (
-//     <div>
-//       <Link to={"/admin"}>
-//         <ReactSVG
-//           className="admin-home-icon"
-//           src="../../src/assets/svg/home.svg"
-//           alt=""
-//         />
-//       </Link>
-//       <div className="admin-camping-list-title">캠핑장 리스트</div>
-//       <div className="admin-camping-list-wrapper">
-//         {campingPlaces.map((campingPlace) => (
-//           <div key={campingPlace.id} className="camping-list">
-//             <Link to={`/admin/camp-fix/${campingPlace.id}`}>
-//               <div className="admin-camping-name-list">
-//                 <div>
-//                   {campingPlace.name}{" "}
-//                   <div className="admin-camping-date-list">
-//                     {campingPlace.createdAt}
-//                   </div>
-//                 </div>
-//                 <ReactSVG
-//                   className="list-pencil"
-//                   src="../../src/assets/svg/pencil.svg"
-//                   alt=""
-//                 />
-//               </div>
-//             </Link>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AdminCampingListPage;
