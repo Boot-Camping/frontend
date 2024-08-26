3d2a36e3c2fc08854546d276c8747aac0caafa06
hbin3673 <hbin3673@gmail.com>
2024-08-27 00:34:20 +0900

    [AdminCampingListPage.jsx] 캠핑장 리스트를 get으로 서버에서 받아와서 사용하도록 수정

--- src/pages/AdminCampingListPage.jsx
+++ src/pages/AdminCampingListPage.jsx
@@ -1,108 +1,43 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { ReactSVG } from "react-svg";
-// import { filterData } from "../utils/filterData";
-// import { useLoadMore } from "../hooks/useLoadMore";
-// import { noticeData } from "../constants/notice";
-// import useAllCampInfo from "../hooks/useAllCampInfo";
-// import useCampInfo from "../hooks/useCampInfo";
-// import SaveMoreBtn from "../components/save-page/SaveMoreBtn";
-
-// const AdminCampingListPage = () => {
-//   const [filter, setFilter] = useState("all");
-//   const filteredItems = filterData(noticeData, filter, "noticeStatus");
-//   const { visibleItems, loadMore, hasMoreItems } = useLoadMore(
-//     3,
-//     filteredItems
-//   );
-//   const { campInfos, loading, error } = useAllCampInfo(); // campId 제거
-
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div>캠핑장 정보 가져오기 실패: {error.message}</div>;
-//   }
-
-//   return (
-//     <div>
-//       <Link to={"/admin"}>
-//         <ReactSVG
-//           className="admin-home-icon"
-//           src="../../src/assets/svg/home.svg"
-//           alt=""
-//         />
-//       </Link>
-//       <div className="admin-camping-list-title">캠핑장 리스트</div>
-//       <div className="admin-camping-list-wrapper">
-//         {campInfos.map((campInfo) => (
-//           <div key={campInfo.id} className="camping-list">
-//             <Link to={`/admin/camp-fix/${campInfo.id}`}>
-//               <div className="admin-camping-name-list">
-//                 <div>
-//                   {campInfo.name}{" "}
-//                   <div className="admin-camping-date-list">
-//                     {campInfo.createdAt}
-//                   </div>
-//                 </div>
-//                 <ReactSVG
-//                   className="list-pencil"
-//                   src="../../src/assets/svg/pencil.svg"
-//                   alt=""
-//                 />
-//               </div>
-//             </Link>
-//           </div>
-//         ))}
-//       </div>
-//       {hasMoreItems && (
-//         <SaveMoreBtn onClick={loadMore} hasMoreItems={hasMoreItems} />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default AdminCampingListPage;
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ReactSVG } from "react-svg";
-import { saveData } from "../constants/save";
 import { filterData } from "../utils/filterData";
 import { useLoadMore } from "../hooks/useLoadMore";
 import { noticeData } from "../constants/notice";
-import SaveMoreBtn from "../components/save-page/SaveMoreBtn";
+import useFetchCampingList from "../hooks/useFetchCampingList";
 import useCampingPlaceFilter from "../hooks/useCampingPlaceFilter";
+import AdminMainLink from "../components/admin-camping-register-page/AdminMainLink";
+import SaveMoreBtn from "../components/save-page/SaveMoreBtn";
 
 const AdminCampingListPage = () => {
+  const { campingPlaces, error } = useFetchCampingList();
   const { selectedFilter, setSelectedFilter, campingPlaceFiltered } =
-    useCampingPlaceFilter(saveData);
+    useCampingPlaceFilter(campingPlaces);
+
   const [filter, setFilter] = useState("all");
   const filteredItems = filterData(noticeData, filter, "noticeStatus");
   const { visibleItems, loadMore, hasMoreItems } = useLoadMore(
     3,
     filteredItems
   );
+
+  if (error) {
+    return <div>캠핑장 정보 가져오기 실패: {error.message}</div>;
+  }
+
   return (
     <div>
-      <Link to={"/admin"}>
-        <ReactSVG
-          className="admin-home-icon"
-          src="../../src/assets/svg/home.svg"
-          alt=""
-        />
-      </Link>
+      <AdminMainLink />
       <div className="admin-camping-list-title">캠핑장 리스트</div>
-      <div className="admin-camping-list-wraper">
+      <div className="admin-camping-list-wrapper">
         {campingPlaceFiltered.map((campingPlace) => (
           <div key={campingPlace.id} className="camping-list">
             <Link to={`/admin/camp-fix/${campingPlace.id}`}>
               <div className="admin-camping-name-list">
                 <div>
-                  {campingPlace.campName}{" "}
+                  {campingPlace.name}{" "}
                   <div className="admin-camping-date-list">
-                    {campingPlace.onDate}
+                    {campingPlace.createdAt}
                   </div>
                 </div>
                 <ReactSVG
