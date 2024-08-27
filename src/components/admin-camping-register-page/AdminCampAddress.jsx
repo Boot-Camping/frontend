6855f11feb3af2c6f9b28e05e55565679f306583
hbin3673 <hbin3673@gmail.com>
2024-08-27 15:06:49 +0900

    [AdminCampAddress.jsx] PostCodeAddress 주소 변경

--- src/components/admin-camping-register-page/AdminCampAddress.jsx
+++ src/components/admin-camping-register-page/AdminCampAddress.jsx
@@ -1,54 +1,6 @@
-// import React, { useState } from "react";
-// import useAddress from "../../hooks/useAddress";
-// import PostCodeAddress from "../admin-camping-register-page/PostCodeAddress";
-
-// const SIGN_UP = [{ key: "1", label: "주소", type: "text", placeholder: "" }];
-
-// const AdminCampAddress = ({ setError, setIsOpened }) => {
-//   const { postcode, address } = useAddress();
-
-//   const submitHandle = (event) => {
-//     event.preventDefault();
-
-//     if (postcode === "" || address === "") {
-//       setError(true);
-//       setIsOpened(true);
-//       return;
-//     }
-
-//     setError(false); // 에러가 없으면 메시지 초기화
-//     setIsOpened(false);
-//     console.log("제출 완료");
-//   };
-
-//   return (
-//     <>
-//       <form id="signup-form" onSubmit={submitHandle}>
-//         {SIGN_UP.map((signup) => (
-//           <div className="signup-input-wrap" key={signup.key}>
-//             <label className="signup-input-label">{signup.label}</label>
-//             {signup.label === "주소" ? (
-//               <PostCodeAddress />
-//             ) : (
-//               <input
-//                 className="signup-input"
-//                 type={signup.type}
-//                 placeholder={signup.placeholder}
-//                 required
-//               />
-//             )}
-//           </div>
-//         ))}
-//       </form>
-//     </>
-//   );
-// };
-
-// export default AdminCampAddress;
-
 import React from "react";
 import useAddress from "../../hooks/useAddress";
-import PostCodeAddress from "../admin-camping-register-page/PostCodeAddress";
+import PostCodeAddress from "./PostCodeAddress";
 
 const SIGN_UP = [{ key: "1", label: "주소", type: "text", placeholder: "" }];
 
