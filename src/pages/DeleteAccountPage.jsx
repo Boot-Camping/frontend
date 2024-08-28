6c4f719356e6ca32d416084ebf56b0fc8124c1bb
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-28 23:24:59 +0900

    [DeleteAccountPage.jsx] 회원탈퇴 DELETE 요청 및 CSS 수정

--- src/pages/DeleteAccountPage.jsx
+++ src/pages/DeleteAccountPage.jsx
@@ -3,19 +3,37 @@ import "../components/delete-account-page/DeleteAccountPage.css";
 import DeleteAccountInfo from "../components/delete-account-page/DeleteAccountInfo";
 import DeleteConfirm from "../components/delete-account-page/DeleteConfirm";
 import DeleteAccountModal from "../components/delete-account-page/DeleteAccountModal";
+import { getUserIdFromToken } from "../utils/getUserIdFromToken";
+import { deleteRequest } from "../utils/api";
+import EmptyContent from "../components/common/EmptyContent";
+import { useNavigate } from "react-router-dom";
 
 const DeleteAccountPage = () => {
+  const { accessToken } = getUserIdFromToken();
   const [isOpened, setIsOpened] = useState(false);
   const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
+  const idRef = useRef(null);
   const passwordRef = useRef(null);
   const checkboxRef = useRef(null);
 
-  const deleteHandle = (event) => {
+  const deleteHandle = async (event) => {
     event.preventDefault();
 
+    const customHeaders = {
+      Authorization: accessToken,
+    };
+
+    const loginId = idRef.current.value;
     const password = passwordRef.current.value;
     const isChecked = checkboxRef.current.checked;
+
+    if (!loginId) {
+      idRef.current.focus();
+      return;
+    }
 
     if (!password) {
       passwordRef.current.focus();
@@ -28,15 +46,39 @@ const DeleteAccountPage = () => {
       return;
     }
 
-    console.log("탈퇴 완료", { password });
-    setIsOpened(true);
-    setError(false);
+    const account = {
+      loginId: loginId,
+      password: password,
+    };
+
+    try {
+      await deleteRequest("user/delete", account, customHeaders);
+      setError(false);
+      setErrorMessage("");
+      setIsOpened(true);
+      localStorage.removeItem("accessToken");
+
+      navigate("/");
+      window.location.reload();
+    } catch (error) {
+      setError(true);
+      setErrorMessage(error.message);
+      setIsOpened(false);
+    }
   };
 
   return (
     <section className="delete-account-wrap">
       <DeleteAccountInfo />
-      <DeleteConfirm passwordRef={passwordRef} checkboxRef={checkboxRef} />
+      <DeleteConfirm
+        idRef={idRef}
+        passwordRef={passwordRef}
+        checkboxRef={checkboxRef}
+      />
+
+      {errorMessage && (
+        <EmptyContent errorMessage={errorMessage} error={error} />
+      )}
 
       <button className="delete-account-btn" onClick={deleteHandle}>
         회원 탈퇴
