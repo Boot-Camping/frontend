bff4b1528978c49faa3432c3a17d99ffd687f465
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-28 16:34:10 +0900

    [EmptyContent.jsx] 에러 문구 띄우기

--- src/components/common/EmptyContent.jsx
+++ src/components/common/EmptyContent.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./EmptyContent.css";
 
-const EmptyContent = ({ errorMessage }) => {
+const EmptyContent = ({ errorMessage, error }) => {
   const responseMessage = errorMessage.includes("Message: ")
     ? errorMessage.split("Message: ")[1]
     : "내역이 존재하지 않습니다";
 
   return (
-    <div className="empty-content">
+    <div className={`${!error ? "empty-content" : "error-message"}`}>
       <div>{responseMessage}</div>
     </div>
   );
