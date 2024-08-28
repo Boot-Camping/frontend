051edbbbbe2df986328a260e75c89bf6c23950a4
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-28 22:19:09 +0900

    [filterType.js] 결제내역 페이지 예약취소 조건 추가

--- src/constants/filterType.js
+++ src/constants/filterType.js
@@ -10,6 +10,11 @@ export const filterType = {
       type: "이용 완료",
       class: "usage-filter",
     },
+    {
+      status: "CANCEL",
+      type: "예약 취소",
+      class: "usage-filter",
+    },
   ],
   cash: [
     {
