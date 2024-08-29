719ddbe58c3dd0b634a895947021570fe36937a6
aa1as <sm13929881@gmail.com>
2024-08-29 15:41:15 +0900

    [MainCampingList.jsx] 스와이퍼 사용하여 한 페이지당 10개의 캠핑정보만 보이게 슬라이더 기능 추가 및 css 변경

--- src/components/main-page/MainCampingList.jsx
+++ src/components/main-page/MainCampingList.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/pagination";
 import "../main-page/MainCampingList.css";
 import { ReactSVG } from "react-svg";
 import useHeartClick from "../../hooks/useHeartClick";
@@ -18,6 +22,8 @@ const MainCampingList = () => {
 
   const { heartClick, heartClickHandler } = useHeartClick([]);
 
+  const slidesPerPage = 10;
+
   return (
     <>
       <div className="camping-title-wrapper">
@@ -33,56 +39,77 @@ const MainCampingList = () => {
         </select>
       </div>
 
-      <div className="camping-list-wrapper">
-        {campingPlaceFiltered.map((campingPlace, index) => (
-          <div key={campingPlace.id} className="camping-list">
-            <Link to={`/camping/detail/${campingPlace.id}`}>
-              <img
-                className="camping-img"
-                src={campingPlace.imageUrls}
-                alt={campingPlace.name}
-              />
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {Array.from({
+          length: Math.ceil(campingPlaceFiltered.length / slidesPerPage),
+        }).map((_, pageIndex) => (
+          <SwiperSlide key={pageIndex}>
+            <div className="camping-list-wrapper">
+              {campingPlaceFiltered
+                .slice(
+                  pageIndex * slidesPerPage,
+                  (pageIndex + 1) * slidesPerPage
+                )
+                .map((campingPlace, index) => (
+                  <div key={campingPlace.id} className="camping-list">
+                    <Link to={`/camping/detail/${campingPlace.id}`}>
+                      <img
+                        className="camping-img"
+                        src={campingPlace.imageUrls}
+                        alt={campingPlace.name}
+                      />
 
-              <ReactSVG
-                className={`camping-img-heart ${
-                  !heartClick[index] && "camping-img-heart-delete"
-                }`}
-                src={svgCollection.heart}
-                alt=""
-                onClick={(e) => {
-                  e.preventDefault();
-                  heartClickHandler(index);
-                }}
-              />
+                      <ReactSVG
+                        className={`camping-img-heart ${
+                          !heartClick[index] && "camping-img-heart-delete"
+                        }`}
+                        src={svgCollection.heart}
+                        alt=""
+                        onClick={(e) => {
+                          e.preventDefault();
+                          heartClickHandler(index);
+                        }}
+                      />
 
-              <div className="camping-name">{campingPlace.name}</div>
-              <div className="camping-sub-title-wrapper">
-                <div className="camping-type">
-                  {campingPlace.categories.join(". ")}
-                </div>
-                <div className="camping-price">{campingPlace.price}원</div>
-              </div>
-            </Link>
-            <div className="camping-info-icons-wrapper">
-              <div className="camping-info-star-wrapper">
-                <ReactSVG
-                  className="camping-info-star"
-                  src={svgCollection.stars}
-                  alt=""
-                />
-                <div className="camping-info">{campingPlace.averageGrade}</div>
-                <div className="camping-info">
-                  ・리뷰({campingPlace.reviewCount})
-                </div>
-              </div>
+                      <div className="camping-name">{campingPlace.name}</div>
+                      <div className="camping-sub-title-wrapper">
+                        <div className="camping-type">
+                          {campingPlace.categories.join(". ")}
+                        </div>
+                        <div className="camping-price">
+                          {campingPlace.price}원
+                        </div>
+                      </div>
+                    </Link>
+                    <div className="camping-info-icons-wrapper">
+                      <div className="camping-info-star-wrapper">
+                        <ReactSVG
+                          className="camping-info-star"
+                          src={svgCollection.stars}
+                          alt=""
+                        />
+                        <div className="camping-info">
+                          {campingPlace.averageGrade}
+                        </div>
+                        <div className="camping-info">
+                          ・리뷰({campingPlace.reviewCount})
+                        </div>
+                      </div>
 
-              <div className="camping-info">
-                예약자 수({campingPlace.reservedDateCount})
-              </div>
+                      <div className="camping-info">
+                        예약자 수({campingPlace.reservedDateCount})
+                      </div>
+                    </div>
+                  </div>
+                ))}
             </div>
-          </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </>
   );
 };
