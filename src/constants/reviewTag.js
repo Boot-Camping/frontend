a75f0ba1490f8406b2c5f157d9e3a69ceea4684e
Sammyissewon <person1341@gmail.com>
2024-08-29 14:17:25 +0700

    [ReviewWriter.jsx] formData 데이터 변환 코드 추가

--- src/constants/reviewTag.js
+++ src/constants/reviewTag.js
@@ -1,32 +1,22 @@
 export const reviewTag = [
   {
     id: 1,
-    label: "청결해요",
-    className: "tag-good clean",
+    label: "KIND",
+    className: "tag-good",
   },
   {
     id: 2,
-    label: "접근성 좋아요",
-    className: "tag-good near",
+    label: "GOOD",
+    className: "tag-good",
   },
   {
     id: 3,
-    label: "친절해요",
-    className: "tag-good friendly",
+    label: "EXCELLENT",
+    className: "tag-good",
   },
   {
     id: 4,
-    label: "지저분해요",
-    className: "tag-bad dirty",
-  },
-  {
-    id: 5,
-    label: "접근성 나빠요",
-    className: "tag-bad far",
-  },
-  {
-    id: 6,
-    label: "불친절해요",
-    className: "tag-bad unfriendly",
+    label: "BAD",
+    className: "tag-bad",
   },
 ];
