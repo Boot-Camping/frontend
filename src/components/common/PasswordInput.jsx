4c2793dbfd67b4a485e22b6cbfaaab31016ccdab
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-08-30 17:40:11 +0900

    [PasswordInput.jsx] ref props로 받기

--- src/components/common/PasswordInput.jsx
+++ src/components/common/PasswordInput.jsx
@@ -3,16 +3,19 @@ import "./PasswordInput.css";
 import { ReactSVG } from "react-svg";
 import { svgCollection } from "../../constants/svgCollection";
 
-const PasswordInput = ({ inputClass, type }) => {
+const PasswordInput = ({ inputClass, type, passwordRef }) => {
   const [showPassword, setShowPassword] = useState(false);
 
   const showClickHandle = () => {
     setShowPassword(!showPassword);
   };
 
-	const placeholder = type === "pwChk" ? "비밀번호를 다시 한번 입력해주세요" : "숫자, 특수문자를 포함한 8자 이상의 비밀번호";
+  const placeholder =
+    type === "pwChk"
+      ? "비밀번호를 다시 한번 입력해주세요"
+      : "숫자, 특수문자를 포함한 8자 이상의 비밀번호";
 
-	const name = type === "pwChk" ? "passwordChk" : "password";
+  const name = type === "pwChk" ? "passwordChk" : "password";
 
   return (
     <>
@@ -23,6 +26,7 @@ const PasswordInput = ({ inputClass, type }) => {
           placeholder={placeholder}
           name={name}
           required
+          ref={passwordRef}
         />
         <ReactSVG
           src={!showPassword ? svgCollection.views : svgCollection.eyeSlash}
