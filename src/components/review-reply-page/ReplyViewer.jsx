2ace724ed3a16c80380b061729d0937e3def79fb
Sammyissewon <person1341@gmail.com>
2024-08-31 19:34:09 +0700

    [ReplyViewer.jsx] console.log 추가

--- src/components/review-reply-page/ReplyViewer.jsx
+++ src/components/review-reply-page/ReplyViewer.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./ReviewPage.css";
 import useReplyViewer from "../../hooks/useReplyViewer";
-import ReplyWriter from "./ReplyWriter";
+import { formatDate } from "../../utils/formatDate";
 
 const ReplyViewer = ({ reviewId }) => {
-  const { replies, loading, error } = useReplyViewer(reviewId);
-  console.log("reviewId:", reviewId);
+  const { replies, loading, error, deleteReply } = useReplyViewer(reviewId);
 
   if (loading) {
     return <div>Loading replies...</div>;
@@ -15,19 +14,33 @@ const ReplyViewer = ({ reviewId }) => {
     return <div>댓글 가져오기 실패: {error.message}</div>;
   }
 
+  const deleteReplyHandle = async (replyId) => {
+    console.log("삭제하려는 댓글 id:", replyId);
+    try {
+      await deleteReply(replyId);
+      console.log(`Reply with ID ${replyId} deleted successfully`);
+    } catch (error) {
+      console.error("댓글 삭제에 실패했습니다:", error);
+    }
+  };
+
   return (
     <div>
-      {replies.map((reply, index) => (
-        <div key={index} className="reply-box">
+      {replies.map((reply) => (
+        <div key={reply.replyId} className="reply-box">
           <div className="reply-upper-box">
             <div className="reply-writer-box">
               <div className="reply-id">{reply.userLoginId}</div>
-              <div className="reply-date">{reply.createdAt}</div>
+              <div className="reply-date">{formatDate(reply.createdAt)}</div>
             </div>
 
             <div className="reply-edit-btns">
-              {/* <button className="reply-edit-btn">수정</button> */}
-              <button className="reply-delete-btn">삭제</button>
+              <button
+                className="reply-delete-btn"
+                onClick={() => deleteReplyHandle(reply.id)}
+              >
+                삭제
+              </button>
             </div>
           </div>
 
--- src/hooks/useReplyViewer.js
+++ src/hooks/useReplyViewer.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getUserIdFromToken } from "../utils/getUserIdFromToken";
 import { get } from "../utils/api";
+import { deleteRequest } from "../utils/api";
 
 const useReplyViewer = (reviewId) => {
   const [replies, setReplies] = useState([]);
@@ -29,7 +30,29 @@ const useReplyViewer = (reviewId) => {
     fetchReplies();
   }, [reviewId]);
 
-  return { replies, loading, error };
+  const deleteReply = async (replyId) => {
+    const customHeaders = {
+      Authorization: `${accessToken}`,
+    };
+
+    try {
+      await deleteRequest(
+        `review/${reviewId}/replies/${replyId}`,
+        {},
+        customHeaders
+      );
+      // 삭제된 댓글을 목록에서 제거
+      setReplies((prevReplies) =>
+        prevReplies.filter((reply) => reply.id !== replyId)
+      );
+      console.log(`댓글 ID${replyId} 삭제에 성공했습니다!`);
+    } catch (error) {
+      console.error("댓글 삭제에 실패했습니다:", error);
+      setError(error);
+    }
+  };
+
+  return { replies, loading, error, deleteReply };
 };
 
 export default useReplyViewer;
