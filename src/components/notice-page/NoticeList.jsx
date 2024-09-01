14f3ea7bbbef027997fd6884b54286f3a9ffc986
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-09-01 11:59:53 +0900

    [NoticePage.jsx] 주석삭제 및 에러메시지 추가

--- src/components/notice-page/NoticeList.jsx
+++ src/components/notice-page/NoticeList.jsx
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 import { shortDateDot } from "../../utils/shortDateDot";
 import { ReactSVG } from "react-svg";
 import { svgCollection } from "../../constants/svgCollection";
-import { filterData } from "../../utils/filterData";
+import EmptyContent from "../common/EmptyContent";
 
 const NoticeList = ({
   noticeData,
+	errorMessage,
   linkPrefix = "/notice",
   svgSrc = svgCollection.prev,
 }) => {
   return (
     <div className="notice-list-wrap">
-      {noticeData.map((data, index) => (
+      {noticeData.length > 0 ? (
+				noticeData.map((data, index) => (
         <Link
           to={`${linkPrefix}/${data.id}`}
           key={`notice-list-${index + 1}`}
@@ -21,20 +23,16 @@ const NoticeList = ({
         >
           <div className="notice-info">
             <div className="notice-list-title">
-              {/* <div
-                className={`notice-status ${
-                  data.noticeStatus === "이벤트" ? "notice-event" : ""
-                }`}
-              >
-                {data.noticeStatus}
-              </div> */}
               <div>{data.title}</div>
             </div>
             <div className="notice-date">{shortDateDot(data)}</div>
           </div>
           <ReactSVG src={svgSrc} className="notice-list-img" />
         </Link>
-      ))}
+      ))
+		) : (
+			<EmptyContent errorMessage={errorMessage} />
+		)}
     </div>
   );
 };
--- src/pages/NoticePage.jsx
+++ src/pages/NoticePage.jsx
@@ -1,83 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import "../components/notice-page/NoticePage.css";
-// import "../components/notice-page/NoticeFilter.css";
-// import { Link } from "react-router-dom";
-// import { ReactSVG } from "react-svg";
-// import NoticeList from "../components/notice-page/NoticeList";
-// import { svgCollection } from "../constants/svgCollection";
-// import Filter from "../components/common/Filter";
-// import { filterType } from "../constants/filterType";
-// import { get } from "../utils/api";
-// import Pagination from "../components/common/Pagination";
-
-// const NoticePage = () => {
-//   const [noticeData, setNoticeData] = useState([]);
-//   const [page, setPage] = useState(0);
-//   const size = 8;
-//   const [totalPages, setTotalPages] = useState(0);
-//   // const [filter, setFilter] = useState("all");
-
-//   // const filterChangeHandle = (status) => {
-//   //   setFilter(status);
-//   // };
-
-//   useEffect(() => {
-//     const getNoticeData = async () => {
-//       const customHeaders = {
-//         "Content-Type": "application/x-www-form-urlencoded",
-//       };
-
-//       const params = {
-//         page: page,
-//         size: size,
-//       };
-
-//       const queryString = new URLSearchParams(params).toString();
-
-//       try {
-//         const response = await get(
-//           `admin/notice/all?${queryString}`,
-//           customHeaders
-//         );
-//         setNoticeData(response.content);
-//         console.log(response.content);
-//         setTotalPages(response.totalPages);
-//       } catch (error) {
-//         console.log(error.message);
-//       }
-//     };
-
-//     getNoticeData();
-//   }, [page]);
-
-//   return (
-//     <section className="notice-page-wrap">
-//       <div className="notice-title-wrap">
-//         <Link to={"/mypage"}>
-//           <ReactSVG src={svgCollection.prev} className="notice-move-prev" />
-//         </Link>
-//         <div>공지사항</div>
-//       </div>
-
-//       {/* <Filter
-//         filterChangeHandle={filterChangeHandle}
-//         filterType={filterType.notice}
-//         wrapClassName="notice-page-filter"
-//         allClassName="event-filter"
-//       /> */}
-
-//       <NoticeList
-//         noticeData={noticeData}
-//         // filter={filter}
-//       />
-
-//       <Pagination page={page} setPage={setPage} totalPages={totalPages} />
-//     </section>
-//   );
-// };
-
-// export default NoticePage;
-
 import React, { useEffect, useState } from "react";
 import "../components/notice-page/NoticePage.css";
 import "../components/notice-page/NoticeFilter.css";
@@ -90,11 +10,12 @@ import Pagination from "../components/common/Pagination";
 
 const NoticePage = ({
   linkPrefix = "/notice",
-  listSvgSrc = svgCollection.prev, // NoticeList에서 사용할 SVG
-  pageSvgSrc = svgCollection.prev, // NoticePage에서 사용할 SVG
+  listSvgSrc = svgCollection.prev,
+  pageSvgSrc = svgCollection.prev,
 }) => {
   const navigate = useNavigate();
   const [noticeData, setNoticeData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
   const [page, setPage] = useState(0);
   const size = 8;
   const [totalPages, setTotalPages] = useState(0);
@@ -118,10 +39,9 @@ const NoticePage = ({
           customHeaders
         );
         setNoticeData(response.content);
-        console.log(response.content);
         setTotalPages(response.totalPages);
       } catch (error) {
-        console.log(error.message);
+        setErrorMessage(error.message);
       }
     };
 
@@ -132,20 +52,23 @@ const NoticePage = ({
     <section className="notice-page-wrap">
       <div className="notice-title-wrap">
         <ReactSVG
-          src={pageSvgSrc} // NoticePage에서 사용하는 SVG
+          src={pageSvgSrc}
           className="notice-move-prev"
           onClick={() => navigate(-1)}
         />
         <div>공지사항</div>
       </div>
 
-      <NoticeList
-        noticeData={noticeData}
-        linkPrefix={linkPrefix}
-        svgSrc={listSvgSrc} // NoticeList에서 사용하는 SVG
-      />
+      <div className="notice-content">
+        <NoticeList
+          noticeData={noticeData}
+          errorMessage={errorMessage}
+          linkPrefix={linkPrefix}
+          svgSrc={listSvgSrc}
+        />
 
-      <Pagination page={page} setPage={setPage} totalPages={totalPages} />
+        <Pagination page={page} setPage={setPage} totalPages={totalPages} />
+      </div>
     </section>
   );
 };
