03eaa9ac5c1d09790cad1f855a6937a7294c5290
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-09-02 15:49:41 +0900

    [api.js] api 요청 배포사이트로 보내고 배포서버로 리다이렉트

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
+const API_BASE_URL = import.meta.env.VITE_API_PROXY || "/api";
 
 const apiRequest = async (method, endpoint, data = {}, customHeaders = {}) => {
   const headers = {
