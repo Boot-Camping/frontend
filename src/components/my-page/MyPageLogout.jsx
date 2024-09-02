24cb8baab89df475c05f9a9d773e18781c057ad8
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-09-02 16:54:52 +0900

    [MyPageLogout.jsx] 엑세스 토큰 추가 및 포탈모달 컴포넌트로 변경

--- src/components/my-page/MyPageLogout.jsx
+++ src/components/my-page/MyPageLogout.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import "./MyPageLogout.css";
-import { createPortal } from "react-dom";
 import { closeModal } from "../../utils/closeModal";
 import { getUserIdFromToken } from "../../utils/getUserIdFromToken";
 import { useNavigate } from "react-router-dom";
+import { post } from "../../utils/api";
+import PortalModal from "../common/PortalModal";
+import EmptyContent from "../common/EmptyContent";
 
-const MyPageLogout = () => {
+const MyPageLogout = ({ errorMessage, setErrorMessage }) => {
+  const { accessToken } = getUserIdFromToken();
   const [isOpened, setIsOpened] = useState(false);
+  const [error, setError] = useState(false);
   const navigate = useNavigate();
 
   const logoutModalHandle = (event) => {
@@ -42,34 +46,28 @@ const MyPageLogout = () => {
 
       <>
         {isOpened && (
-          <>
-            {createPortal(
-              <div className="overlay" onClick={closeModal(setIsOpened)}></div>,
-              document.getElementById("overlay-root")
-            )}
-            {createPortal(
-              <div className="logout-modal modal">
-                <div className="logout-txt">
-                  <div>로그아웃 하시겠습니까?</div>
-                  <div className="logout-btn-wrap">
-                    <button
-                      className="logout-cancel-btn"
-                      onClick={closeModal(setIsOpened)}
-                    >
-                      취소
-                    </button>
-                    <button
-                      className="logout-confirm-btn"
-                      onClick={logoutHandle}
-                    >
-                      확인
-                    </button>
-                  </div>
+          <PortalModal setIsOpened={setIsOpened}>
+            <div className="logout-modal modal">
+              <div className="logout-txt">
+                <div>로그아웃 하시겠습니까?</div>
+                <div className="logout-btn-wrap">
+                  <button
+                    className="logout-cancel-btn"
+                    onClick={closeModal(setIsOpened)}
+                  >
+                    취소
+                  </button>
+                  <button className="logout-confirm-btn" onClick={logoutHandle}>
+                    확인
+                  </button>
                 </div>
-              </div>,
-              document.getElementById("modal-root")
-            )}
-          </>
+
+                {error && (
+                  <EmptyContent errorMessage={errorMessage} error={error} />
+                )}
+              </div>
+            </div>
+          </PortalModal>
         )}
       </>
     </>
