c3b03a395fa334b402074cd39cfa215e1384ff06
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-09-02 14:39:52 +0900

    [MyPageLogou.jsx] POST 요청 삭제

--- src/components/my-page/MyPageLogout.jsx
+++ src/components/my-page/MyPageLogout.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import "./MyPageLogout.css";
+import { createPortal } from "react-dom";
 import { closeModal } from "../../utils/closeModal";
 import { getUserIdFromToken } from "../../utils/getUserIdFromToken";
 import { useNavigate } from "react-router-dom";
-import { post } from "../../utils/api";
-import PortalModal from "../common/PortalModal";
-import EmptyContent from "../common/EmptyContent";
 
-const MyPageLogout = ({ errorMessage, setErrorMessage }) => {
-  const { accessToken } = getUserIdFromToken();
+const MyPageLogout = () => {
   const [isOpened, setIsOpened] = useState(false);
-  const [error, setError] = useState(false);
   const navigate = useNavigate();
 
   const logoutModalHandle = (event) => {
@@ -19,23 +15,14 @@ const MyPageLogout = ({ errorMessage, setErrorMessage }) => {
     setIsOpened(true);
   };
 
-  const logoutHandle = async () => {
-    const customHeaders = {
-      Authorization: accessToken,
-    };
+  const logoutHandle = (event) => {
+    event.preventDefault();
 
-    try {
-      await post("user/logout", {}, customHeaders);
-      setIsOpened(false);
+    setIsOpened(false);
 
-      localStorage.removeItem("accessToken");
-      navigate("/");
-      window.location.reload();
-    } catch (error) {
-      setErrorMessage(error.message);
-      setError(true);
-      console.log(error);
-    }
+    localStorage.removeItem("accessToken");
+    navigate("/");
+    window.location.reload();
   };
 
   return (
@@ -46,28 +33,34 @@ const MyPageLogout = ({ errorMessage, setErrorMessage }) => {
 
       <>
         {isOpened && (
-          <PortalModal setIsOpened={setIsOpened}>
-            <div className="logout-modal modal">
-              <div className="logout-txt">
-                <div>로그아웃 하시겠습니까?</div>
-                <div className="logout-btn-wrap">
-                  <button
-                    className="logout-cancel-btn"
-                    onClick={closeModal(setIsOpened)}
-                  >
-                    취소
-                  </button>
-                  <button className="logout-confirm-btn" onClick={logoutHandle}>
-                    확인
-                  </button>
+          <>
+            {createPortal(
+              <div className="overlay" onClick={closeModal(setIsOpened)}></div>,
+              document.getElementById("overlay-root")
+            )}
+            {createPortal(
+              <div className="logout-modal modal">
+                <div className="logout-txt">
+                  <div>로그아웃 하시겠습니까?</div>
+                  <div className="logout-btn-wrap">
+                    <button
+                      className="logout-cancel-btn"
+                      onClick={closeModal(setIsOpened)}
+                    >
+                      취소
+                    </button>
+                    <button
+                      className="logout-confirm-btn"
+                      onClick={logoutHandle}
+                    >
+                      확인
+                    </button>
+                  </div>
                 </div>
-
-                {error && (
-                  <EmptyContent errorMessage={errorMessage} error={error} />
-                )}
-              </div>
-            </div>
-          </PortalModal>
+              </div>,
+              document.getElementById("modal-root")
+            )}
+          </>
         )}
       </>
     </>
