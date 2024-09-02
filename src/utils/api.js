7717aa24c3dddf59fa54e07443ad628559a68af1
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-09-02 16:06:38 +0900

    [api.js] 백엔드 서버로 다시 연결

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_PROXY;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const apiRequest = async (method, endpoint, data = {}, customHeaders = {}) => {
   const headers = {
