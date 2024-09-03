b65c7a1b9f38bc3b5ae5820ced88500f4ad5e402
aa1as <sm13929881@gmail.com>
2024-09-04 02:12:35 +0900

    [CategoryCampingList.jsx] 카테고리별로 캠핑장 정보 불러오는 훅 사용

--- src/components/category-page/CategoryCampingList.jsx
+++ src/components/category-page/CategoryCampingList.jsx
@@ -1,28 +1,32 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "../category-page/CategoryCampingList.css";
 import { ReactSVG } from "react-svg";
 import useCampingPlaceFilter from "../../hooks/useCampingPlaceFilter";
-import useFetchCampingList from "../../hooks/useFetchCampingList";
+import useCategoryFilter from "../../hooks/useCategoryFilter";
 import { svgCollection } from "../../constants/svgCollection";
 import useWishlist from "../../hooks/useWishlist";
 
 const CategoryCampingList = () => {
   const { category } = useParams();
-  const { campingPlaces, error } = useFetchCampingList();
+  const categoryTitle = category || "전체";
+
+  const params = {
+    category: categoryTitle !== "전체" ? categoryTitle : undefined,
+  };
+
+  const queryString = new URLSearchParams(params).toString();
+
+  const { campingPlaces, error } = useCategoryFilter(
+    queryString,
+    categoryTitle
+  );
   const { isSaved, toggleWishlist } = useWishlist(campingPlaces);
 
-  const categoryTitle = category || "전체";
-  const categoryfilter = campingPlaces.filter(
-    (place) =>
-      categoryTitle === "전체" || place.categories.includes(categoryTitle)
-  );
-
   const { selectedFilter, setSelectedFilter, campingPlaceFiltered } =
-    useCampingPlaceFilter(categoryfilter);
+    useCampingPlaceFilter(campingPlaces);
 
   if (error) return <div>Error: {error}</div>;
-
   return (
     <>
       <div className="category-title-wrapper">
@@ -41,62 +45,66 @@ const CategoryCampingList = () => {
 
       {campingPlaceFiltered.map((campingPlace, index) => (
         <div key={campingPlace.id} className="category-camping-list">
-          <img
-            className="category-camping-img"
-            src={campingPlace.imageUrls[0] || "default-image-url.jpg"}
-            alt=""
-          />
+          <Link to={`/camping/detail/${campingPlace.id}`}>
+            <img
+              className="category-camping-img"
+              src={campingPlace.imageUrls[0] || "default-image-url.jpg"}
+              alt=""
+            />
 
-          <ReactSVG
-            className={`category-camping-img-heart ${
-              !isSaved[index] && "category-camping-img-heart-delete"
-            }`}
-            src={svgCollection.heart}
-            alt=""
-            onClick={(e) => {
-              e.preventDefault();
-              toggleWishlist(index, campingPlace);
-            }}
-          />
+            <ReactSVG
+              className={`category-camping-img-heart ${
+                !isSaved[index] && "category-camping-img-heart-delete"
+              }`}
+              src={svgCollection.heart}
+              alt=""
+              onClick={(e) => {
+                e.preventDefault();
+                toggleWishlist(index, campingPlace);
+              }}
+            />
 
-          <div className="category-camping-type">
-            {campingPlace.categories.join(", ")}
-          </div>
-          <div className="category-camping-sub-title-wrapper">
-            <div className="category-camping-name">{campingPlace.name}</div>
-            <div className="category-camping-price">{campingPlace.price}원</div>
-          </div>
-          <div className="category-camping-addr-wrapper">
-            <div className="category-camping-addr-icon-wrapper">
-              <ReactSVG
-                className="category-camping-addr-icon"
-                src={svgCollection.location}
-                alt="위치"
-              />
-              <div className="category-camping-addr">{campingPlace.addr}</div>
+            <div className="category-camping-type">
+              {campingPlace.categories.join(", ")}
             </div>
-            <div className="category-camping-people">
-              기준인원/{campingPlace.standardNum}
-            </div>
-          </div>
-          <div className="category-camping-info-icons-wrapper">
-            <div className="category-camping-info-star-wrapper">
-              <ReactSVG
-                className="category-camping-info-star"
-                src={svgCollection.stars}
-                alt=""
-              />
-              <div className="category-camping-info">
-                {campingPlace.averageGrade}
-              </div>
-              <div className="category-camping-info">
-                ・리뷰({campingPlace.reviewCount})
+            <div className="category-camping-sub-title-wrapper">
+              <div className="category-camping-name">{campingPlace.name}</div>
+              <div className="category-camping-price">
+                {campingPlace.price}원
               </div>
             </div>
-            <div className="category-camping-info">
-              예약({campingPlace.reservedDateCount})
+            <div className="category-camping-addr-wrapper">
+              <div className="category-camping-addr-icon-wrapper">
+                <ReactSVG
+                  className="category-camping-addr-icon"
+                  src={svgCollection.location}
+                  alt="위치"
+                />
+                <div className="category-camping-addr">{campingPlace.addr}</div>
+              </div>
+              <div className="category-camping-people">
+                기준인원/{campingPlace.standardNum}
+              </div>
             </div>
-          </div>
+            <div className="category-camping-info-icons-wrapper">
+              <div className="category-camping-info-star-wrapper">
+                <ReactSVG
+                  className="category-camping-info-star"
+                  src={svgCollection.stars}
+                  alt=""
+                />
+                <div className="category-camping-info">
+                  {campingPlace.averageGrade}
+                </div>
+                <div className="category-camping-info">
+                  ・리뷰({campingPlace.reviewCount})
+                </div>
+              </div>
+              <div className="category-camping-info">
+                예약({campingPlace.reservedDateCount})
+              </div>
+            </div>
+          </Link>
         </div>
       ))}
     </>
@@ -104,3 +112,8 @@ const CategoryCampingList = () => {
 };
 
 export default CategoryCampingList;
+
+// const categoryfilter = campingPlaces.filter(
+//   (place) =>
+//     categoryTitle === "전체" || place.categories.includes(categoryTitle)
+// );
