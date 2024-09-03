51a8d03b29e4529ffc0fce1026801d87b3c05c98
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-09-03 22:42:17 +0900

    [ChatModal.jsx] 채팅방 클릭시 ChatList 대신 ChatRoom 컴포넌트 띄우기

--- src/components/chatting/ChatModal.jsx
+++ src/components/chatting/ChatModal.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChatModal.css";
 import { createPortal } from "react-dom";
 import { ReactSVG } from "react-svg";
 import { svgCollection } from "../../constants/svgCollection";
 import ChatList from "./ChatList";
+import ChatRoom from "./ChatRoom";
 
 const ChatModal = ({ isOpen, animating, onClose }) => {
+  const [join, setJoin] = useState(false);
+  const [currentId, setCurrentId] = useState(null);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  const joinHandle = (id) => {
+    setCurrentId(id);
+    setJoin(true);
+  };
+
   return (
     <>
       {createPortal(
@@ -19,7 +30,26 @@ const ChatModal = ({ isOpen, animating, onClose }) => {
             className="chat-close-btn"
             onClick={onClose}
           />
-          <ChatList />
+          {!join ? (
+            <ChatList
+              isOpen={isOpen}
+              setJoin={setJoin}
+              joinHandle={joinHandle}
+              error={error}
+              setError={setError}
+              errorMessage={errorMessage}
+              setErrorMessage={setErrorMessage}
+            />
+          ) : (
+            <ChatRoom
+              setJoin={setJoin}
+              currentId={currentId}
+              error={error}
+              setError={setError}
+              errorMessage={errorMessage}
+              setErrorMessage={setErrorMessage}
+            />
+          )}
         </div>,
         document.getElementById("modal-root")
       )}
