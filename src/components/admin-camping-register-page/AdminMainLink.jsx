ea258e453a6e2fe8bf4b9e7f9b1383014de72672
hbin3673 <hbin3673@gmail.com>
2024-09-14 20:33:23 +0900

    [AdminMainLink.jsx] svgCollection.js 에서 불러와서 사용하는 것으로 경로 변경

--- src/components/admin-camping-register-page/AdminMainLink.jsx
+++ src/components/admin-camping-register-page/AdminMainLink.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactSVG } from "react-svg";
+import { svgCollection } from "../../constants/svgCollection";
 import "../admin-camping-register-page/AdminCampingRegister.css";
 
 const AdminMainLink = () => {
   return (
     <div>
       <Link to={"/admin"}>
-        <ReactSVG
-          className="admin-home-icon"
-          src="../../src/assets/svg/home.svg"
-          alt=""
-        />
+        <ReactSVG className="admin-home-icon" src={svgCollection.home} alt="" />
       </Link>
     </div>
   );
